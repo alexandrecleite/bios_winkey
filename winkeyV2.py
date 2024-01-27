c6e575c8e0f1f627fc81d3edae6dce7aee98a97b
Alexandre C. Leite <62979484+alexandrecleite@users.noreply.github.com>
2024-01-27 04:04:55 -0300

    Add files via upload
    New version. Rewrite all code.

---
+++ winkeyV2.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python
+import sys, re
+
+if len(sys.argv) < 2:
+    print('\nVocê deve especificar o nome do arquivo. Ex: "\33[1;33mpython winkey.py bios.bin\33[0;40m"\n')
+    sys.exit()
+
+with open(sys.argv[1], 'rb') as f:
+    s = f.read()
+    for match in re.finditer(b'\x01\x00\x00\x00\x00\x00\x00\x00\x01\x00\x00\x00\x00\x00\x00\x00\x1d\x00\x00', s):
+        pos = match.span()
+        f.seek(pos[1])
+        result = str(f.read(30))
+print('\nA chave de ativação é:\33[1;33m ', result[6:35],'\n\33[0;40m')
+

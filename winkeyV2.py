fa11f9326ae63ee4288d6b593a0ecfb317b7b20a
Alexandre C. Leite <62979484+alexandrecleite@users.noreply.github.com>
2024-01-27 04:32:09 -0300

    Add files via upload

--- winkeyV2.py
+++ winkeyV2.py
@@ -7,9 +7,9 @@ if len(sys.argv) < 2:
 
 with open(sys.argv[1], 'rb') as f:
     s = f.read()
-    for match in re.finditer(b'\x01\x00\x00\x00\x00\x00\x00\x00\x01\x00\x00\x00\x00\x00\x00\x00\x1d\x00\x00', s):
+    for match in re.finditer(b'\x01\x00\x00\x00\x00\x00\x00\x00\x01\x00\x00\x00\x00\x00\x00\x00\x1d\x00\x00\x00', s):
         pos = match.span()
         f.seek(pos[1])
-        result = str(f.read(30))
-print('\nA chave de ativação é:\33[1;33m ', result[6:35],'\n\33[0;40m')
+        result = str(f.read(29))
+print('\nA chave de ativação é:\33[1;33m ', result[2:31],'\n\33[0;40m')
 

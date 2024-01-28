d81b57e255e421a0a019998631d813939d95eecf
Alexandre C. Leite <62979484+alexandrecleite@users.noreply.github.com>
2024-01-28 17:07:29 -0300

    Add files via upload

---
+++ winkeyGUIV2.py
@@ -0,0 +1,40 @@
+#!/usr/bin/python
+import PySimpleGUI as sg 
+import sys, re   
+
+def engine(arquivo):
+   with open(arquivo, 'rb') as f:
+       s = f.read()
+       for match in re.finditer(b'\x01\x00\x00\x00\x00\x00\x00\x00\x01\x00\x00\x00\x00\x00\x00\x00\x1d\x00\x00', s):
+           pos = match.span()
+           f.seek(pos[1])
+           result = str(f.read(30))
+           serialnum = 'A chave de ativação é:', result[6:35]
+           return serialnum
+
+sg.theme('SystemDefault') 
+   
+layout = [[sg.Text('Por favor, selecione o arquivo:'), 
+           sg.Text(size=(15,1), key='-OUTPUT-')], 
+          [sg.Input(), sg.FileBrowse(button_text="Selecionar", file_types=(("Bios Files", "*.bin *.rom"),("All Files", "*.*")))],
+          [sg.Button('Verificar'), sg.Button('Sair')]]
+  
+window = sg.Window('WINKEY v2 - by ACL', layout) 
+  
+while True: 
+    event, values = window.read() 
+      
+    if event in  (None, 'Sair'): 
+        break
+      
+    if event == 'Verificar': 
+        arquivo = values[0] 
+        if arquivo == "" or engine(arquivo) == None:
+            sg.popup('Chave não encontrada.\nPor favor, selecione outro arquivo.', font=('Arial 10 bold'))
+        else:
+#                print(engine(arquivo))
+                arquivo = values[0]
+                sg.popup(font=('Arial 10 bold'), text_color=('blue'), *engine(arquivo))
+
+window.close()
+
